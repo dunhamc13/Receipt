d4cb808700c1a7eb2d4a753fad50da9f39e6e511
OlympianCode <johnmcmahon427@gmail.com>
2018-06-19 23:44:27 -0700

    recreating git repo

---
+++ ReceiptTransformer/ReceiptTransformerv0-7.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ReceiptTransformer
+{
+    partial class frmReceiptTransformer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTransform = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnTransform
+            // 
+            this.btnTransform.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.btnTransform.Location = new System.Drawing.Point(12, 408);
+            this.btnTransform.Name = "btnTransform";
+            this.btnTransform.Size = new System.Drawing.Size(172, 56);
+            this.btnTransform.TabIndex = 4;
+            this.btnTransform.Text = "&Transform";
+            this.btnTransform.UseVisualStyleBackColor = true;
+            this.btnTransform.Click += new System.EventHandler(this.btnTransform_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(503, 39);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(488, 361);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(909, 403);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(62, 13);
+            this.lblCount.TabIndex = 7;
+            this.lblCount.Text = "x total items";
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopy.Location = new System.Drawing.Point(503, 406);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(166, 56);
+            this.btnCopy.TabIndex = 6;
+            this.btnCopy.Text = "&Copy DPCIs";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.AllowDrop = true;
+            this.txtInput.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(12, 39);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInput.Size = new System.Drawing.Size(485, 361);
+            this.txtInput.TabIndex = 2;
+            this.txtInput.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtFilePath_DragDrop);
+            this.txtInput.DragOver += new System.Windows.Forms.DragEventHandler(this.txtFilePath_DragOver);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnClear.Location = new System.Drawing.Point(244, 418);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(119, 35);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "Text Files|*.txt";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(369, 10);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(134, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "&Open Transaction...";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.AllowDrop = true;
+            this.txtFilePath.Location = new System.Drawing.Point(12, 12);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(351, 20);
+            this.txtFilePath.TabIndex = 0;
+            this.txtFilePath.DragDrop += new System.Windows.Forms.DragEventHandler(this.txtFilePath_DragDrop);
+            this.txtFilePath.DragOver += new System.Windows.Forms.DragEventHandler(this.txtFilePath_DragOver);
+            this.txtFilePath.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtFilePath_KeyDown);
+            this.txtFilePath.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFilePath_KeyPress);
+            this.txtFilePath.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtFilePath_KeyUp);
+            // 
+            // frmReceiptTransformer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 476);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnTransform);
+            this.Name = "frmReceiptTransformer";
+            this.Text = "Receipt Transformer - Version 0.7";
+            this.Load += new System.EventHandler(this.frmReceiptTransformer_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnTransform;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtFilePath;
+    }
+}
+
---
+++ ReceiptTransformer/ReceiptTransformerv0-7.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace ReceiptTransformer
+{
+    public partial class frmReceiptTransformer : Form
+    {
+        string[] lines;
+        public frmReceiptTransformer()
+        {
+            InitializeComponent();
+        }
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+            openFileDialog1.ShowDialog();
+            txtFilePath.Text = openFileDialog1.FileName;
+            OpenFile(txtFilePath.Text);
+        }
+
+        private void OpenFile(string path)
+        {
+            try { 
+                lines = System.IO.File.ReadAllLines(path); // get file into string array
+            } catch (ArgumentException ae)
+            {
+                //don't do anything, stop processing
+                return;
+            }
+            foreach (string line in lines)
+            {
+                txtInput.Text += line + "\r\n";
+            }
+            Transform();
+        }
+
+        private void btnTransform_Click(object sender, EventArgs e)
+        {
+            Transform();
+        }
+
+        private void Transform()
+        {
+
+            if (lines == null)
+                lines = txtInput.Text.Split('\n'); // get text into string array
+            if (lines.Length == 0)
+                MessageBox.Show("Please open a transaction file or paste the contents in.");
+            List<string> dpcis = new List<string>(); //init list to catch dpcis
+            int count = 0;//init count
+            for (int i = 0; i < lines.Length; i++)//for every line of the receipt
+            {
+                try
+                {
+                    string thisLine = lines[i];//iteration variable
+                    Regex pattern = new Regex(@"\d{3}:[SK]\d{9}"); //regex pattern
+                    if (pattern.IsMatch(thisLine.Substring(0, 14)))//if this line has a dpci
+                    {
+                        //grab the dpci
+                        string thisDpci = thisLine.Substring(5, 3) + "-" + thisLine.Substring(8, 2) + "-" + thisLine.Substring(10, 4);
+                        //if this line is not voiding a dpci
+                        if (!thisLine.Contains("*VOID LINE*"))
+                        {
+                            //add it and increment the counter
+                            dpcis.Add(thisDpci);
+                            count++;
+                        }
+                        else
+                        {//else remove it and decrement the counter
+                            dpcis.Remove(thisDpci);
+                            count--;
+                        }
+                    }
+                }
+                catch (ArgumentOutOfRangeException aoore)
+                {
+                    continue;
+                }
+            }
+            //now we've got all the dpcis, prepare the output
+            string output = "";
+            for (int i = 0; i < dpcis.Count; i++)
+            {
+                //for all but the last add a space
+                if (i < dpcis.Count - 1)
+                    output += dpcis[i] + " ";
+                else
+                    output += dpcis[i];
+            }
+            if (count == 0)
+            {
+                MessageBox.Show("No DPCIs found.  \n\nPlease make sure you've entered the right data. \nIf the data is good and it's still not working, tell John about it.");
+            }
+            txtOutput.Text = output;
+            lblCount.Text = count.ToString() + " total items";
+            lines = null;
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            txtOutput.SelectAll();
+            txtOutput.Copy();
+        }
+
+        private void frmReceiptTransformer_Load(object sender, EventArgs e)
+        {
+            lblCount.Text = "";
+            txtInput.Focus();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ResetForm();
+        }
+
+        private void ResetForm()
+        {
+            txtFilePath.Clear();
+            txtInput.Clear();
+            txtOutput.Clear();
+            txtInput.Focus();
+            lblCount.Text = "";
+        }
+
+        //https://social.msdn.microsoft.com/Forums/vstudio/en-US/487b9ffd-246a-4e5f-a388-6a623d7d63a5/adding-dragdrop-to-a-text-box?forum=csharpgeneral
+
+        private void txtFilePath_DragDrop(object sender, DragEventArgs e)
+        {
+            ResetForm();
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                if (System.IO.File.Exists(files[0]))
+                {
+                    this.txtFilePath.Text = files[0];
+                    OpenFile(this.txtFilePath.Text);
+                    Transform();
+                }
+            }
+        }
+
+        private void txtFilePath_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void txtFilePath_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (txtFilePath.Text.Length == 0)
+                    btnOpen.PerformClick();
+                else
+                {
+                    string p = txtFilePath.Text;
+                    ResetForm();
+                    txtFilePath.Text = p;
+                    OpenFile(p);
+                }
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void txtFilePath_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void txtFilePath_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
